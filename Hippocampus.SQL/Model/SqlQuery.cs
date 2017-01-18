f069f8f726a349780d6790623fe2d0cd3dbdf6a0
Johnny Anderson dos Santos <johnny_santos@hotmail.com.br>
2017-01-18 17:00:19 -0200

    Created a new type of return

---
+++ Hippocampus.SQL/Model/SqlQuery.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace HippocampusSql.Model
+{
+    /// <summary>
+    /// Information of generated SQL Query
+    /// </summary>
+    public class SqlQuery
+    {
+        internal SqlQuery()
+        {
+        }
+
+        /// <summary>
+        /// Text content
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Named parameters
+        /// </summary>
+        public IDictionary<string, object> NamedParameters { get; set; }
+
+        /// <summary />
+        public static implicit operator string(SqlQuery q)
+            => q.Content;
+    }
+}
