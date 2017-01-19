964485ac22bc1329609270e24c0c4572c62238dd
Johnny Anderson dos Santos <johnny_santos@hotmail.com.br>
2017-01-19 08:23:59 -0200

    Pushed alpha 0.0.1.3

--- nuget-build.bat
+++ nuget-build.bat
@@ -1,3 +1,3 @@
 @echo off
-nuget pack Hippocampus.SQL.csproj -Symbols -Verbosity detailed -Build -Prop Configuration=Release
+nuget pack Hippocampus.SQL/Hippocampus.SQL.csproj -Symbols -Verbosity detailed -Build -Prop Configuration=Release
 exit 0
